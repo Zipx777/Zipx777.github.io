157204e08c6556aeaae9b83e84a756950c0f1165
Zipx777 <steven@chalmersfamily.com>
2018-07-11 22:01:21 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+<div id="header_wrap" class="outer">
+        <header class="inner">
+          <a id="forkme_banner" href="https://github.com/Zipx777">View on GitHub</a>
+
+          <h1 id="project_title">Zipx777's homepage</h1>
+          <h2 id="project_tagline">fun stuff arriving slowly</h2>
+
+        </header>
+		
+		<div id="outerNavbar">
+			<div id="innerNavbar">
+				<a href="index.html"><div class="navbarOption">TicTacToe</div></a>
+				<a href="mptest.html"><div class="navbarOption">MP Test</div></a>	
+				<a href="bumpers.html"><div class="navbarOption">Bumpers</div></a>
+				<a href="circles.html"><div class="navbarOption">Circles</div></a>
+				<a href="gameoflife.html"><div class="navbarOption">Game of Life</div></a>
+				<a href="patterns.html"><div class="navbarOption">Patterns</div></a>
+			</div>
+		</div>
+    </div>
+?>
