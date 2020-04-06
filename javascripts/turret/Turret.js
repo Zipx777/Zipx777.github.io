95249512a3543b913f260d59fae82805b19bdf6d
Zipx777 <steven@chalmersfamily.com>
2020-04-06 00:18:04 -0700

    Add color change on firing pattern

--- javascripts/turret/Turret.js
+++ javascripts/turret/Turret.js
@@ -2,105 +2,113 @@
 var Turret = function(startX, startY, startRadius, startRotationSpeed, startFacingVector) {
 	var x = startX,
 		y = startY,
-		color = "orangered",
+		color = "green", //main color
+		currentColor = color, //color currently being rendered
+		prefireColor = "orange",
+		firingColor = "red",
 		radius = startRadius || 12,
 		rotationSpeed = startRotationSpeed || 1,
 		facingVector = startFacingVector || new Vector(1,0),
 		playerDirection = new Vector(1,0),
+		targetInFrontAngle = 45,
 		delayBetweenShots = 5,
-		burstLength = 5,
+		burstLength = 10,
 		currentShotsFiredInBurstCount = 0,
 		firingDelay = 100,
 		tickCount = 0,
 		lastShotFiredTick = 0,
 		playerFirstSeenTick = 0,
-		aimedAtTarget = false,
 		targetInFront = false;
-		
+
 	//return value of x
 	var getX = function() {
 		return x;
 	};
-		
+
 	//return value of y
 	var getY = function() {
 		return y;
 	};
-	
+
 	var getFacingVector = function() {
 		return facingVector;
 	}
-	
+
 	var getVectorToPlayer = function() {
 		return vectorToPlayer;
 	}
-	
+
 	//set new value for x
 	var setX = function(newX) {
 		x = newX;
 	};
-	
+
 	//set new value for y
 	var setY = function(newY) {
 		y = newY;
 	};
-	
+
 	var setFacingVector = function(newFacingVector) {
 		facingVector = newFacingVector;
 	}
-	
+
 	var setVectorToPlayer = function(newVectorToPlayer) {
 		vectorToPlayer = newVectorToPlayer.normalize();
 	}
-	
-	//update Turret rotation/facing
+
+	//update Turret rotation/facing/firing
 	var update = function(playerX, playerY, projectiles) {
 		tickCount += 1;
 		var vectorToPlayer = new Vector(playerX - x, playerY - y);
 		playerDirection = vectorToPlayer.normalize();
-		
+
 		var playerAngle = playerDirection.toAngle();
 		var turretAngle = facingVector.toAngle();
-		
+
 		var turretDotPlayer = facingVector.dot(playerDirection);
 		var turretCrossPlayer = facingVector.cross(playerDirection);
 		var signedAngleBetween = Math.atan2(turretCrossPlayer, turretDotPlayer);
 		//math.atan2(cross/dot)
-		
+
+		//##########################################
+		//###########  UPDATE FACING  ##############
+		//##########################################
+
 		if (Math.abs(signedAngleBetween) * 180 / Math.PI < rotationSpeed) {
+			//snap to target if aim is less than <rotationSpeed> away to avoid flickering
 			facingVector = playerDirection;
 			if (!targetInFront) {
 				playerFirstSeenTick = tickCount;
 			}
-			aimedAtTarget = true;
 			targetInFront = true;
 		} else {
-			aimedAtTarget = false;
-			if (Math.abs(signedAngleBetween) < (45 * Math.PI / 180)) {
+			if (Math.abs(signedAngleBetween) < (targetInFrontAngle * Math.PI / 180)) {
 				if (!targetInFront) {
-				playerFirstSeenTick = tickCount;
+					playerFirstSeenTick = tickCount;
 				}
 				targetInFront = true;
 			} else {
 				targetInFront = false;
 			}
-			var angleChange = 0;
-			if (signedAngleBetween > 0) {
-				angleChange = rotationSpeed;
-			} else if (signedAngleBetween < 0) {
-				angleChange = -1 * rotationSpeed;
-			}
-			
+
+			var angleChange = (signedAngleBetween / Math.abs(signedAngleBetween)) * rotationSpeed;
+
 			var newTurretAngle = turretAngle + (angleChange * Math.PI / 180);
-			
+
 			var newFacingVector = new Vector(Math.cos(newTurretAngle), Math.sin(newTurretAngle));
-			
+
 			facingVector = newFacingVector.normalize();
 		}
-		
+
+		//##########################################
+		//###########  UPDATE FIRING  ##############
+		//##########################################
+
 		if (targetInFront) {
+			currentColor = prefireColor;
 			if (tickCount - playerFirstSeenTick > firingDelay) {
 				if (currentShotsFiredInBurstCount < burstLength) {
+					currentColor = firingColor;
 					if (tickCount - lastShotFiredTick > delayBetweenShots) {
 						lastShotFiredTick = tickCount;
 						currentShotsFiredInBurstCount += 1;
@@ -111,6 +119,9 @@ var Turret = function(startX, startY, startRadius, startRotationSpeed, startFaci
 					playerFirstSeenTick = tickCount;
 				}
 			}
+		} else {
+			currentColor = color;
+			currentShotsFiredInBurstCount = 0;
 		}
 	}
 
@@ -118,20 +129,20 @@ var Turret = function(startX, startY, startRadius, startRotationSpeed, startFaci
 	var draw = function(ctx) {
 		ctx.save();
 		ctx.translate(x,y);
-		
+
 		var angle = facingVector.toAngle();
 		ctx.rotate(angle);
-		
-		ctx.fillStyle = color;
+
+		ctx.fillStyle = currentColor;
 		ctx.beginPath();
 		ctx.moveTo(2 * radius, 0);
 		ctx.lineTo(-1 * radius, -1 * radius);
 		ctx.lineTo(-1 * radius, radius);
 		ctx.fill();
-		
+
 		ctx.restore();
 	};
-	
+
 	// Define which variables and methods can be accessed
 	return {
 		getX: getX,
