88ef9c5c232c98a752ee2caf2c0440c2e3a558fe
Zipx777 <steven@chalmersfamily.com>
2020-06-30 00:09:44 -0700

    Added contained rule
    New rule for "contained" pattern

--- javascripts/gameoflife/exampleRules.js
+++ javascripts/gameoflife/exampleRules.js
@@ -56,3 +56,10 @@ var decayingStrobingBlobArr = [
 	[0,1,7]
 ];
 exampleRules.set("Strobe Decay", decayingStrobingBlobArr);
+
+//Contained pulsing blob
+var containedPulsingBlobArr = [
+	[0,1,2,3,4,6,7,8],
+	[1,3,4,8]
+];
+exampleRules.set("Contained", containedPulsingBlobArr);
