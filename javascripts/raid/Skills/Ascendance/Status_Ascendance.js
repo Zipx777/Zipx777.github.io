aa7b57d6b351f98c49a4576843d19ee69336742d
Zipx777 <steven@chalmersfamily.com>
2021-11-29 15:05:43 -0800

    adjust ascendance buff
    buff stormstrike cooldown reduction to line up better with 2 gcds

--- javascripts/raid/Skills/Ascendance/Status_Ascendance.js
+++ javascripts/raid/Skills/Ascendance/Status_Ascendance.js
@@ -4,7 +4,7 @@ class Status_Ascendance extends Status {
 		super();
 		this.name = "Status_Ascendance";
 		this.duration = 900;
-		this.stormstrikeCooldownMultiplier = 0.5;
+		this.stormstrikeCooldownMultiplier = 0.4;
 		this.stormstrikeDamageMultiplier = 1.5;
 		this.autoAttackDamageMultiplier = 1.5;
 		this.meleeRange = 300;
